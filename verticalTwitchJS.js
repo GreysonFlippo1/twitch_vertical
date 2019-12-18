64eb7cfad5fff1b97fa644ed705d4e07702f0d4a
GreysonFlippo1 <greysonflippo@gmail.com>
2019-12-17 21:38:38 -0600

    added js that can detect orientation change and call a seperate function once if it has

--- verticalTwitchJS.js
+++ verticalTwitchJS.js
@@ -1,1 +1,26 @@
-alert("twitch!");
+
+let vertical = 0;
+let width = 0;
+let height = 0;
+
+setInterval(checkOrientation,100);
+
+function checkOrientation(){
+    width = window.innerWidth;
+    height = window.innerHeight;
+    let vertical_old = vertical;
+    if(height>=width){
+        vertical = 1;
+    }
+    else{
+        vertical = 0;
+    }
+    if(vertical != vertical_old){
+        updateGUI();
+    }
+}
+
+function updateGUI(){
+    
+}
+
