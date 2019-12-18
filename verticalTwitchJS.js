cbb202d25c1dd85df3fb08d875c81341158d67d9
GreysonFlippo1 <greysonflippo@gmail.com>
2019-12-18 00:10:31 -0600

    chat reverts back to it's original position when page is in horizontal move on load and on resize

--- verticalTwitchJS.js
+++ verticalTwitchJS.js
@@ -10,17 +10,37 @@ setInterval(checkOrientation,100);
 function checkOrientation(){
     width = window.innerWidth;
     height = window.innerHeight;
-    //let vertical_old = vertical;
+    let vertical_old = vertical;
+
+    if(chat && vertical == 1){
+        chat.children[0].classList.remove("tw-hide");
+        chat.style.width="100%";
+        chat.style.height="50vh";
+        chat.style.paddingBottom="75px";
+        chat.children[0].classList.remove("tw-hide");
+        chat.children[0].style.height="50vh";
+        chat.children[0].children[0].children[0].style.width="100%";
+        document.getElementsByClassName("channel-root__right-column")[0].style.width="100%";
+    }
+    else if(chat && vertical == 0){
+        chat.style.width="350px";
+        chat.style.height="100%";
+        chat.style.paddingBottom="0px";
+        chat.children[0].style.height="100%";
+        chat.children[0].children[0].children[0].style.width="350px";
+        document.getElementsByClassName("channel-root__right-column")[0].style.width="350px";
+    }
+
     if(height>=width){
         vertical = 1;
     }
     else{
         vertical = 0;
     }
-    // if(vertical != vertical_old){
-    //     updateGUI();
-    // }
-    updateGUI();
+
+    if(vertical != vertical_old){
+        updateGUI();
+    }
 }
 
 function updateGUI(){
@@ -28,33 +48,33 @@ function updateGUI(){
         vertical = -1;
     }
     else{
-        let twFlex = document.getElementsByClassName("tw-flex");
-        for(let i = 0; i < twFlex.length; i++){
-            twFlex[i].classList.add(`verticalTTV_${i}`);
-        }
-        //console.log(document.getElementsByClassName("verticalTTV_27")[0].children[2]); //Chat element
-        if(!chat && document.getElementsByClassName("verticalTTV_27")[0].children[2] && document.getElementsByClassName("verticalTTV_27")[0].children[2].id){
-            let chatID = document.getElementsByClassName("verticalTTV_27")[0].children[2].id
-            //chat = document.getElementById(chatID);
-            chat=document.getElementsByClassName("verticalTTV_27")[0].removeChild(document.getElementById(chatID));
-            //chat.style.width="100px";
-        }
-        if(chat){
-            //document.getElementsByClassName("verticalTTV_27")[0].removeChild[2];
-            if(!about){
-                about = document.getElementsByClassName("channel-root")[0].removeChild(document.getElementsByClassName("tw-pd-x-2")[0]);
-                document.getElementsByClassName("channel-root")[0].appendChild(chat);
-                document.getElementsByClassName("channel-root")[0].appendChild(about);
+        if(vertical == 1){
+            let twFlex = document.getElementsByClassName("tw-flex");
+            for(let i = 0; i < twFlex.length; i++){
+                twFlex[i].classList.add(`verticalTTV_${i}`);
             }
-            chat.style.width="100%";
-            chat.style.height="50vh";
-            chat.style.paddingBottom="75px";
-            chat.children[0].classList.remove("tw-hide");
-            chat.children[0].style.height="50vh";
-            document.getElementsByClassName("verticalTTV_86")[0].style.width="100%";
-            //chat.children[0].style.display="block!important";
+    
+            if(!chat && document.getElementsByClassName("verticalTTV_27")[0].children[2] && document.getElementsByClassName("verticalTTV_27")[0].children[2].id){
+                let chatID = document.getElementsByClassName("verticalTTV_27")[0].children[2].id;
+                chat=document.getElementsByClassName("verticalTTV_27")[0].removeChild(document.getElementById(chatID));
+            }
+    
+            if(chat){;
+                if(!about){
+                    about = document.getElementsByClassName("channel-root")[0].removeChild(document.getElementsByClassName("tw-pd-x-2")[0]);
+                    document.getElementsByClassName("channel-root")[0].appendChild(chat);
+                    document.getElementsByClassName("channel-root")[0].appendChild(about);
+                }
+            }
         }
-        //element 27 is significant
+        else if(vertical == 0){
+            if(chat){
+                document.getElementsByClassName("verticalTTV_27")[0].appendChild(chat);
+                about=null;
+            }
+            //else do nothing
+        }
     }
+
 }
 
